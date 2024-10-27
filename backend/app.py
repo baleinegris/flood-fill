cfaef321ef96905d2727de3cccd0a6a956c2553f
Tyson Caul <tjcaul@icloud.com>
2024-10-26 22:58:42 -0400

    Move get_evel to util.py and implement endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,25 @@
-import requests
 from flask import Flask, request, redirect
 from flask_cors import CORS
+from main import get_expected_floods, get_plot
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/get-data/<latitude>/<longitude>')
-def getData(latitude, longitude):
+@app.route('/get-data?lat=<latitude>&lat=<longitude>&addr=<address>&scenario=<scenario>')
+def getData(latitude, longitude, address):
     # CALL WHATEVER NEEDS TO BE CALLED
-    data = {}
+    print(latitude, longitude, addr)
+    (lat, lon, scenario, addr) = [request.args.get(x) for x in ['lat', 'lon', 'scenario', 'addr']]
+    expected_floods = get_expected_floods(lat, lon, scenario)
+    plot_data = get_plot(address, expected_floods)
+    data = {'expected_floods': expected_floods, 'plot': plot_data}
     return data
 
+
+@app.route('/ping')
+def ping():
+    return {'ping': 'ping'}
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=1234, debug=True)
--- backend/main.py
+++ backend/main.py
@@ -9,8 +9,6 @@ import fiona
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-
 sample_data = {
     'latitude': np.random.uniform(-90, 90, 1000),
     'longitude': np.random.uniform(-180, 180, 1000),
@@ -36,16 +34,6 @@ def get_plot(address):
     plt.ylabel('Expected Floods')
     plt.grid(True)
     plt.savefig("expected_floods.png")
-
-def get_elevation(lat, lon):
-    api_key = 'YOUR_GOOGLE_MAPS_API_KEY'
-    url = f'https://maps.googleapis.com/maps/api/elevation/json?locations={lat}%2C{lon}&key={GOOGLE_API_KEY}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        results = response.json().get('results')
-        if results:
-            return results[0].get('elevation')
-    return None
 
 def get_lat_long(address):
     return 50, -119
