956ab3cd17c70a1e35b72a6490c20f43567ca4f2
baleinegris <oscar.w.heath@gmail.com>
2024-10-27 01:23:47 -0400

    nothing much

--- flood-fill/src/components/ReportSection.jsx
+++ flood-fill/src/components/ReportSection.jsx
@@ -6,7 +6,7 @@ export default function ReportSection({ precipitation, expectedFloods, graph } )
         <div className="flex items-start h-[40%] justify-start">
         <div className="flex flex-col w-[60%]">
             <div className="text-black"> Average Precipitation: {precipitation}mm</div>
-            <div className="text-black"> Canadian Average: CONST</div>
+            <div className="text-black"> Canadian Average: 537.07mm</div>
             <div className="text-black"> Expected number of floods per year:</div>
             <div className="flex flex-col items-align justify-center">
             Projected number of floods:
